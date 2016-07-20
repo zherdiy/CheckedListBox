abe9e68a17dc7811683b1dfe869ccd6bd85103d9
Alex Zherdiy <alex.zherdiy@gmail.com>
2016-07-20 15:28:05 +0300

    перша фіксація

---
+++ CheckedListBox/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CheckedListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(28, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "bit 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(109, 39);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "bit 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(200, 39);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(46, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "bit 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(177, 101);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.ColumnWidth = 50;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(28, 101);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(143, 64);
+            this.checkedListBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CheckedListBox/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckedListBox
+{
+    public partial class Form1 : Form
+    {
+        static bool isStarted;
+        public Form1()
+        {
+            InitializeComponent();
+            //String[,] myCheckList = new String[2,2] {{ "bit 1", "bit 2", "bit 3" }, { "bit 4", "bit 5", "bit 6" }};
+            String[] myCheckList1 = new String[] { "bit 1", "bit 2", "bit 3" };
+            String[] myCheckList2 = new String[] { "bit 4", "bit 5", "bit 6" };
+            checkedListBox1.Items.AddRange(myCheckList1); // add list to checkedListBox1
+            checkedListBox1.Items.AddRange(myCheckList2);
+            checkedListBox1.CheckOnClick = true; // one click
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isStarted)
+            {
+                btnStart.Text = "STOP";
+                isStarted = true;
+                timer1.Start();
+            }
+            else
+            {
+                btnStart.Text = "START";
+                isStarted = false;
+                timer1.Stop();
+                foreach (Int32 i in checkedListBox1.CheckedIndices)
+                {
+                    checkedListBox1.SetItemChecked(i, false);
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkedListBox1.SetItemChecked(0, true);
+            }
+            else
+            {
+                checkedListBox1.SetItemChecked(0, false);
+            }
+            if (checkBox2.Checked)
+            {
+                checkedListBox1.SetItemChecked(1, true);
+            }
+            else
+            {
+                checkedListBox1.SetItemChecked(1, false);
+            }
+            if (checkBox3.Checked)
+            {
+                checkedListBox1.SetItemChecked(2, true);
+            }
+            else
+            {
+                checkedListBox1.SetItemChecked(2, false);
+            }
+        }
+    }
+}
